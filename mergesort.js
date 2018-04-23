11cac3f561dd54a2499cec82207c0e1e6b7fdf26
AndrewTrahan <globalART19@gmail.com>
2018-04-23 14:46:50 -0500

    mmerge fn complete

--- mergesort.js
+++ mergesort.js
@@ -1,17 +1,26 @@
-function split(array){
+function split(array) {
   //array length/2
   let firstHalf = [];
   let secondHalf = [];
-  let middleInteger = Math.ceil(array.length/2);
-  firstHalf = array.slice(0,middleInteger);
+  let middleInteger = Math.ceil(array.length / 2);
+  firstHalf = array.slice(0, middleInteger);
   secondHalf = array.slice(middleInteger);
-  return [firstHalf,secondHalf];
+  return [firstHalf, secondHalf];
 }
 
-function merge(){
-
+function merge(arr1, arr2) {
+  const mergedArr = [];
+  while (arr1.length > 0 && arr2.length > 0) {
+    if (arr1[0] < arr2[0]) {
+      mergedArr.push(arr1.shift())
+    } else {
+      mergedArr.push(arr2.shift())
+    }
+  }
+  return mergedArr.concat(arr1, arr2)
 }
 
-function mergeSort(){
+function mergeSort(arr) {
+
   return undefined;
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,20 +1,22 @@
 'use strict'
 //forces errors to be thrown.
 
-describe('Split Array function', function(){
-  let array = [4,1,6,8,3,5,8];
+describe('Split Array function', function () {
+  let array = [4, 1, 6, 8, 3, 5, 8];
 
-  beforeEach(function(){
-    spyOn(window,'mergeSort').and.callThrough();
+  beforeEach(function () {
+    spyOn(window, 'mergeSort').and.callThrough();
   });
 
-  it('handles an empty array', function(){
+  it('handles an empty array', function () {
     expect(mergeSort([])).toEqual([]);
-  }); 
-
-  it('splits an array into two arrays', function(){
-    expect(split([1,3,5,2])).toEqual([[1,3],[5,2]]);
   });
 
+  it('splits an array into two arrays', function () {
+    expect(split([1, 3, 5, 2])).toEqual([[1, 3], [5, 2]]);
+  });
+  it('merges 2 sorted arrays into 1 sorted array', function () {
+    expect(merge([1, 3], [2, 5])).toEqual([1, 2, 3, 5])
+  })
 });
 
